cb58cea5606d1a052c5c5ba5bff7880e3eab592e
Ollivier Robert <roberto@keltia.net>
2021-09-27 16:56:16 +0200

    WIP: add clap and empty files.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::{AppSettings, Clap};
+
+mod dice;
+mod roll;
+
+/// Help message
+#[derive(Debug, Clap)]
+#[clap(name = "dices-rs", about = "Small dice utility.")]
+#[clap(version = "0.1.0")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {}
+
+
 fn main() {
+    let opts = Opts::parse();
+
     println!("Hello, world!");
 }
