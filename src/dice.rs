bdbf5d73d268f579cf0131df5e8a19905ea8b871
Ollivier Robert <roberto@keltia.net>
2021-10-04 16:47:23 +0200

    Add biased_dice/internal_roll + test.

--- src/dice.rs
+++ src/dice.rs
@@ -1,3 +1,5 @@
+use rand::prelude::*;
+
 use crate::roll::*;
 use crate::result::Res;
 
@@ -11,8 +13,36 @@ pub enum Dice {
     D100
 }
 
-impl Roll for Dice {
-    fn roll(t: Dice, n: i32) -> Res {
-        todo!()
+struct OpenDice;
+
+fn biased_dice(p: f64) -> bool {
+    let mut rng = rand::thread_rng();
+    let f: f64 = rng.gen();
+    f < p
+}
+
+fn internal_roll(sides: i32) -> i32 {
+    let mut i = 0;
+    loop {
+        if biased_dice(1.0 / (sides - i) as f64) {
+            return i + 1
+        }
+        i += 1;
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_internal_roll() {
+        for i in 0..10 {
+            let r = internal_roll(6);
+
+            assert!(r >= 0 && r <= 6)
+        }
+    }
+}
+
+
