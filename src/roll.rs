062de1d2d86cd8ae19a1afa2e3b69fee6ce5dd20
Ollivier Robert <roberto@keltia.net>
2021-10-05 18:09:59 +0200

    Remove implementation.

--- src/roll.rs
+++ src/roll.rs
@@ -1,14 +1,6 @@
-use crate::dice::Dice;
 use crate::result::Res;
 
 pub trait Roll {
     fn roll(t: Dice, n: i32) -> Res;
 }
-
-impl Roll for Dice {
-    fn roll(t: Dice, n: i32) -> Res {
-        todo!()
-    }
-}
-
 
