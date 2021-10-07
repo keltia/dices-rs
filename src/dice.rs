9e02be6331c01a95639e8246cc9f18cfeec83fd5
Ollivier Robert <roberto@keltia.net>
2021-10-07 18:01:07 +0200

    WIP: do not compile, will be rewritten.

--- src/dice.rs
+++ src/dice.rs
@@ -1,76 +1,46 @@
 
-
 use crate::internal::internal_roll;
 use crate::result::Res;
 use crate::roll::*;
 
-#[derive(Debug, PartialEq)]
-pub enum Dice {
-    D4,
-    D6,
-    D8,
-    D10,
-    D12,
-    D20,
-    D100,
-}
-
-fn is_valid(s: usize) -> Dice {
+fn is_valid(s: usize) -> bool {
     match s {
-        4 => Dice::D4,
-        6 => Dice::D6,
-        8 => Dice::D8,
-        10 => Dice::D10,
-        12 => Dice::D12,
-        20 => Dice::D20,
-        100 => Dice::D100,
+        4|6|8|10|12|20|100 => true,
         _ => panic!("Error: unknown dice: {}", s),
     }
 }
 
-type ConstantDice = usize;
+#[derive(Debug, Eq, PartialEq)]
+pub struct ConstantDice(usize);
 
 impl Roll for ConstantDice {
     fn roll<'a>(&self, r: &'a mut Res) -> &'a mut Res {
-        r.append(*self)
+        r.append(self.0)
     }
 }
 
-#[derive(Debug, PartialEq)]
-pub struct RegDice {
-    kind: Dice,
-    s: usize,
-}
-
-impl RegDice {
-    pub fn new(s: usize) -> Self {
-        Self {
-            kind: is_valid(s),
-            s: s,
-        }
-    }
-}
+/// RegDice is also a usize, only roll() will change
+#[derive(Debug, Eq, PartialEq)]
+pub struct RegDice(usize);
 
 impl Roll for RegDice {
     fn roll<'a>(&self, r: &'a mut Res) -> &'a mut Res {
-        r.append(internal_roll(self.s))
+        r.append(internal_roll(self.0))
     }
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct OpenDice {
-    kind: Dice,
-    d: RegDice,
-    threshold: usize,
+    pub d: RegDice,
+    pub threshold: usize,
 }
 
 impl OpenDice {
-    pub fn new(s: usize) -> Self {
-        OpenDice {
-            kind: is_valid(s),
-            d: RegDice::new(s),
-            threshold: s,
+    pub fn new(s: usize) -> Result<Self,&'static str> {
+        if !is_valid(s) {
+            return Err("bad size");
         }
+        Ok(OpenDice{d: RegDice(s), threshold: s})
     }
 }
 
@@ -84,54 +54,49 @@ impl Roll for OpenDice {
     }
 }
 
-//pub struct Dices<T> {
-//    l: Vec<T>
-//}
+#[derive(Debug, Eq, PartialEq)]
+pub enum Dice {
+    ConstantDice(usize),
+    OpenDice(usize),
+    RegDice(usize)
+}
 
-pub type Dices<T> = Vec<T>;
+impl Roll for Dice {
+    fn roll<'a>(&self, r: &'a mut Res) -> &'a mut Res {
+        let s = r.size;
 
-impl Roll for Dices<RegDice> {
+        let res: &Res = match *self {
+            Dice::ConstantDice(s) => {
+                r.append(self.0);
+            },
+            Dice::RegDice(s) => {
+                r.append(internal_roll(self.0));
+            },
+            Dice::OpenDice(s) => {
+                if r.sum >= self.threshold {
+                    r
+                } else {
+                    r.merge(self.d.roll(&mut Res::new()))
+                }
+            },
+        }
+    }
+
+}
+
+#[derive(Debug)]
+pub struct DiceSet(Vec<Dice>);
+
+impl Roll for DiceSet {
     fn roll<'a>(&self, r: &'a mut Res) -> &'a mut Res {
         let mut r1 = r;
 
-        for dice in self {
+        for dice in &self.0 {
             r1 = dice.roll(r1);
         }
         r1
     }
 }
-
-fn new<T>(s: usize) -> Dices<T> {
-    <T>::new()
-}
-
-/*
-impl Dices<OpenDice> {
-    pub fn new() -> Self {
-        Dices {
-            l: Vec::<OpenDice>::new()
-        }
-    }
-
-    pub fn append(&mut self, iter: impl IntoIterator<Item = OpenDice>) -> &Dices<OpenDice> {
-        for d in iter {
-            self.l.push(d)
-        }
-        self
-    }
-}
-
-impl Roll for Dices<OpenDice> {
-    fn roll<'a>(&self, r: &'a mut Res) -> &'a mut Res {
-        let mut r1 = r;
-
-        for dice in &self.l {
-            r1 = dice.roll(r1);
-        }
-        r1
-    }
-}
-*/
 
 #[cfg(test)]
 mod test {
@@ -141,7 +106,7 @@ mod test {
     fn test_constant() {
         let mut r = Res::new();
 
-        let f: ConstantDice = 0;
+        let f = ConstantDice(0);
         let r = f.roll(&mut r);
 
         println!("{}", r.sum);
@@ -150,7 +115,7 @@ mod test {
 
     #[test]
     fn test_constant_res() {
-        let d: ConstantDice = 0;
+        let d= ConstantDice(0);
         let mut r1 = Res::new();
         r1.sum = 2;
 
@@ -186,14 +151,14 @@ mod test {
 
     #[test]
     fn test_reg_new() {
-        let a = RegDice::new(4);
+        let a = RegDice(4);
 
-        assert_eq!(4, a.s)
+        assert_eq!(4, a.0)
     }
 
     #[test]
     fn test_reg_roll() {
-        let d = RegDice::new(6);
+        let d = RegDice(6);
         let mut r = Res::new();
 
         assert_eq!(0, r.list.len());
@@ -211,7 +176,13 @@ mod test {
     fn test_open_new() {
         let d = OpenDice::new(6);
 
-        assert_eq!(Dice::D6, d.kind);
+        let d = match d {
+            Ok(d) => d,
+            _ => panic!("foo")
+        };
+
+        let v = d.d;
+        assert_eq!(6, v.0);
         assert_eq!(6, d.threshold);
     }
 
@@ -223,7 +194,7 @@ mod test {
 
     #[test]
     fn test_open_roll() {
-        let d = RegDice::new(6);
+        let d = RegDice(6);
         let mut r = Res::new();
 
         assert_eq!(0, r.list.len());
@@ -238,11 +209,23 @@ mod test {
     }
 
     #[test]
-    fn test_dices_reg() {
-        let set= Dices::<RegDice>::new();
+    fn test_dices_const() {
+        let die = Dice::ConstantDice(4);
         let mut r = Res::new();
 
-        let r = set.roll(&mut r);
+        let r = die.roll(&mut r);
+
         println!("{:#?}", r);
+
+        assert_eq!(4, r.sum);
+        assert_eq!(4, r.size);
+        assert_eq!(0, r.bonus);
+    }
+
+    #[test]
+    fn test_dices_reg() {
+        let die = Dice::RegDice(10);
+
+        println!("{:#?}", die);
     }
 }
