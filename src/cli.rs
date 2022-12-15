0319200713b3170a86bba505ff76207250d61a1d
Ollivier Robert <roberto@keltia.net>
2022-12-15 23:58:05 +0100

    Minimal CLI handling.

---
+++ src/cli.rs
@@ -0,0 +1,15 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version, Parser};
+
+/// CLI options
+#[derive(Parser, Debug)]
+#[command(disable_version_flag = true)]
+#[clap(name = crate_name!(), about = crate_description!())]
+#[clap(version = crate_version!(), author = crate_authors!())]
+pub struct Opts {
+    /// Verbose mode.
+    #[clap(short = 'v', long, action = clap::ArgAction::Count)]
+    pub verbose: u8,
+    /// Display utility full version.
+    #[clap(short = 'V', long)]
+    pub version: bool,
+}
