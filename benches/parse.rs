4b41a451f261ed9219eb8bf6a28c97b6dcdda63a
Ollivier Robert <roberto@keltia.net>
2022-12-16 00:30:39 +0100

    Use a const for "dices". Fix imports. Reformat.

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,18 +1,19 @@
-use anyhow::Result;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 use dices_rs::dice::parse::parse_with_bonus;
-use dices_rs::dice::{Dice, DiceSet};
+use dices_rs::dice::DiceSet;
+
+const FAKE_DICE: &str = "234D20 +23";
 
 fn nom_parser(c: &mut Criterion) {
     let mut ds = DiceSet::from_vec(vec![]);
 
-    c.bench_function("nom_parser", |b|
+    c.bench_function("nom_parser", |b| {
         b.iter(|| {
-            let r = parse_with_bonus("234D20 +23").unwrap();
+            let r = parse_with_bonus(FAKE_DICE).unwrap();
             ds = r.1;
-        }
-        ));
+        })
+    });
 
     let _ = ds;
 }
@@ -20,17 +21,13 @@ fn nom_parser(c: &mut Criterion) {
 fn manual_parser(c: &mut Criterion) {
     let mut ds = DiceSet::from_vec(vec![]);
 
-    c.bench_function("manual_parser", |b|
-        b.iter(|| ds = DiceSet::parse("234D20 +23").unwrap()
-        ));
+    c.bench_function("manual_parser", |b| {
+        b.iter(|| ds = DiceSet::parse(FAKE_DICE).unwrap())
+    });
 
     let _ = ds;
 }
 
-criterion_group!(
-    benches,
-    manual_parser,
-    nom_parser,
-);
+criterion_group!(benches, manual_parser, nom_parser,);
 
 criterion_main!(benches);
