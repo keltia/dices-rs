cc429a9fe730133819e0d2f2ae31fae537ae2b05
Ollivier Robert <roberto@keltia.net>
2022-12-21 10:55:16 +0100

    Move command stuff into its own file. Added roll_open() as well.

---
+++ src/cmds.rs
@@ -0,0 +1,77 @@
+use dices_rs::dice::{
+    parse::{parse_open, parse_with_bonus},
+    result::Res,
+    Rollable,
+};
+
+use anyhow::{anyhow, Result};
+use log::{debug, error};
+use nom::{
+    character::complete::{alpha1, space0},
+    combinator::map,
+    sequence::preceded,
+    IResult,
+};
+
+/// List of existing commands
+///
+#[derive(Debug)]
+pub enum Cmd {
+    Doom,
+    Exit,
+    Invalid(String),
+    Move,
+    Open,
+    Roll,
+}
+
+/// Parse a keyword, return the operation
+///
+pub fn parse_keyword(input: &str) -> IResult<&str, Cmd> {
+    let get_op = |s: &str| match s.to_ascii_lowercase().as_str() {
+        "doom" => Cmd::Doom,
+        "dice" => Cmd::Roll,
+        "mouv" => Cmd::Move,
+        "move" => Cmd::Move,
+        "open" => Cmd::Open,
+        "roll" => Cmd::Roll,
+        "exit" => Cmd::Exit,
+        _ => Cmd::Invalid("unknown command".to_string()),
+    };
+    let r = alpha1;
+    map(r, get_op)(input)
+}
+
+/// Generic roller
+///
+pub fn roll_from(input: &str) -> Result<Res> {
+    let ds = match preceded(space0, parse_with_bonus)(input) {
+        Ok((_input, ds)) => {
+            debug!("{:?}", ds);
+            ds
+        }
+        Err(e) => {
+            error!("{:?}", e.to_string());
+            return Err(anyhow!("error parsing input"));
+        }
+    };
+    let res = ds.roll().clone();
+    Ok(res)
+}
+
+/// Generic open dice roller
+///
+pub fn roll_open(input: &str) -> Result<Res> {
+    let d = match preceded(space0, parse_open)(input) {
+        Ok((_input, d)) => {
+            debug!("{:?}", d);
+            d
+        }
+        Err(e) => {
+            error!("{:?}", e.to_string());
+            return Err(anyhow!("error parsing input"));
+        }
+    };
+    let res = d.roll().clone();
+    Ok(res)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod cli;
+mod cmds;
 mod version;
 
 use crate::cli::Opts;
+use crate::cmds::{parse_keyword, roll_from, roll_open, Cmd};
 use crate::version::version;
-
-use dices_rs::dice::{parse::parse_with_bonus, result::Res, Rollable};
 
 use std::path::PathBuf;
 
@@ -12,12 +12,6 @@ use anyhow::{anyhow, Result};
 use clap::Parser;
 use home::home_dir;
 use log::{debug, error, info};
-use nom::{
-    character::complete::{alpha1, space0},
-    combinator::map,
-    sequence::preceded,
-    IResult,
-};
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 use stderrlog::LogLevelNum::{Debug, Info, Trace};
@@ -35,48 +29,6 @@ macro_rules! makepath {
         .iter()
         .collect()
     };
-}
-
-#[derive(Debug)]
-pub enum Cmd {
-    Doom,
-    Exit,
-    Invalid(String),
-    Move,
-    Roll,
-}
-
-/// Parse a keyword, return the operation
-///
-pub fn parse_keyword(input: &str) -> IResult<&str, Cmd> {
-    let get_op = |s: &str| match s.to_ascii_lowercase().as_str() {
-        "doom" => Cmd::Doom,
-        "dice" => Cmd::Roll,
-        "mouv" => Cmd::Move,
-        "move" => Cmd::Move,
-        "roll" => Cmd::Roll,
-        "exit" => Cmd::Exit,
-        _ => Cmd::Invalid("unknown command".to_string()),
-    };
-    let r = alpha1;
-    map(r, get_op)(input)
-}
-
-/// Generic roller
-///
-fn roll_from(input: &str) -> Result<Res> {
-    let ds = match preceded(space0, parse_with_bonus)(input) {
-        Ok((_input, ds)) => {
-            debug!("{:?}", ds);
-            ds
-        }
-        Err(e) => {
-            error!("{:?}", e.to_string());
-            return Err(anyhow!("error parsing input"));
-        }
-    };
-    let res = ds.roll().clone();
-    Ok(res)
 }
 
 /// Main entry point
