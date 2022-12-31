7b9e6332d5e93f9e05902480599cbd41580233d4
Ollivier Robert <roberto@keltia.net>
2022-12-31 15:32:24 +0100

    Bye bye Command::execute(), you won't be missed.

--- src/bin/dices/commands/mod.rs
+++ src/bin/dices/commands/mod.rs
@@ -3,16 +3,6 @@
 
 use std::fmt::Debug;
 
-use anyhow::{anyhow, Result};
-use log::{debug, error, trace};
-use nom::{character::complete::space0, sequence::preceded};
-
-use dices_rs::dice::{
-    parse::{parse_open, parse_with_bonus},
-    result::Res,
-    Rollable,
-};
-
 use crate::commands::core::Cmd;
 
 pub mod aliases;
@@ -35,27 +25,4 @@ pub enum Command {
     Exit,
     /// List all commands
     List,
-}
-
-impl Command {
-    /// Execute the given builtin command
-    ///
-    /// XXX Only builtin or aliases command can be executed here, no parsing context
-    ///
-    pub fn execute(self, input: &str) -> Result<Res> {
-        match self {
-            // Process builtins and aliases
-            //
-            Command::Builtin { cmd, .. } | Command::Alias { cmd, .. } => {
-                trace!("builtin/alias");
-                cmd.execute(input)
-                // match cmd {
-                //     Cmd::Dice => roll_from(input),
-                //     Cmd::Open => roll_open(input),
-                //     _ => Err(anyhow!("invalid command")),
-                //}
-            }
-            _ => Err(anyhow!("should not happen")),
-        }
-    }
 }
