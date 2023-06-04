d367b3517439a773849ef0d8a2f9502d6c7d8095
Ollivier Robert <roberto@keltia.net>
2023-06-04 14:49:57 +0200

    Fix bench by importing the code.

--- benches/random.rs
+++ benches/random.rs
@@ -3,15 +3,44 @@
 
 use criterion::{criterion_group, criterion_main, Criterion};
 use log::debug;
+use rand;
+use rand::Rng;
 
-use dices_rs::dice::internal::{internal_roll, rng_roll};
+/// Head or Tail?
+fn biased_dice(p: f64) -> bool {
+    let mut rng = rand::thread_rng();
+    let f: f64 = rng.gen();
+    f < p
+}
+
+/// Return a roll of a dice of size `sides`
+///
+/// Grows slower as the size of the dice grows
+///
+pub fn internal_roll_loop(sides: usize) -> usize {
+    let mut i = 0;
+    loop {
+        if biased_dice(1.0 / (sides - i) as f64) {
+            return i + 1;
+        }
+        i += 1;
+    }
+}
+
+/// Return a roll of a dice of size `sides`
+///
+/// alternate, `rand` version
+///
+pub fn internal_roll_rng(sides: usize) -> usize {
+    rand::thread_rng().gen_range(1..=sides)
+}
 
 fn internal_roll_d20(c: &mut Criterion) {
     let mut r: usize = 0;
 
     c.bench_function("internal_roll/d20", |b| {
         b.iter(|| {
-            r = internal_roll(20);
+            r = internal_roll_loop(20);
         })
     });
     let _ = r;
@@ -22,7 +51,7 @@ fn rng_roll_d20(c: &mut Criterion) {
 
     c.bench_function("rng_roll/d20", |b| {
         b.iter(|| {
-            r = rng_roll(20);
+            r = internal_roll_rng(20);
         })
     });
     let _ = r;
@@ -33,7 +62,7 @@ fn internal_roll_d100(c: &mut Criterion) {
 
     c.bench_function("internal_roll/d100", |b| {
         b.iter(|| {
-            r = internal_roll(100);
+            r = internal_roll_loop(100);
         })
     });
     let _ = r;
@@ -44,7 +73,7 @@ fn rng_roll_d100(c: &mut Criterion) {
 
     c.bench_function("rng_roll/d100", |b| {
         b.iter(|| {
-            r = rng_roll(100);
+            r = internal_roll_rng(100);
         })
     });
     let _ = r;
