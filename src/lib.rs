65d1c8aaa723b1c9f2b33558a53dbcb134538f8e
Ollivier Robert <roberto@keltia.net>
2025-02-20 22:08:58 +0100

    Remove makepath().

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,3 @@ mod compiler;
 pub mod dice;
 pub mod engine;
 
-/// Simple macro to generate PathBuf from a series of entries
-///
-#[macro_export]
-macro_rules! makepath {
-    ($($item:expr),+) => {
-        [
-        $(PathBuf::from($item),)+
-        ]
-        .iter()
-        .collect()
-    };
-}
