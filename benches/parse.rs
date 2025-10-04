bd9804676fba282d9ad2eb2fba6eda4ed8067d7a
Ollivier Robert <roberto@keltia.net>
2025-10-04 18:02:36 +0200

    Add benchmark timings.

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,7 +1,43 @@
 //! Benchmark the two parsers, the current one based on `nom` and the old, manual one.
 //!
+//! Timing for nom7 and nom8
+//! ```text
+//! NOM 7
+//!
+//! manual_parser           time:   [534.36 ns 540.53 ns 547.82 ns]
+//!                         change: [−24.416% −22.758% −21.112%] (p = 0.00 < 0.05)
+//!                        Performance has improved.
+//! Found 10 outliers among 100 measurements (10.00%)
+//!   6 (6.00%) high mild
+//!   4 (4.00%) high severe
+//!
+//! nom_parser              time:   [197.28 ns 199.09 ns 201.05 ns]
+//!                         change: [−8.3619% −6.6360% −4.9519%] (p = 0.00 < 0.05)
+//!                         Performance has improved.
+//! Found 7 outliers among 100 measurements (7.00%)
+//!   2 (2.00%) low mild
+//!   3 (3.00%) high mild
+//!   2 (2.00%) high severe
+//!
+//!
+//! NOM 8
+//!
+//! manual_parser           time:   [530.22 ns 531.63 ns 533.43 ns]
+//!                         change: [−20.073% −19.687% −19.308%] (p = 0.00 < 0.05)
+//!                         Performance has improved.
+//! Found 4 outliers among 100 measurements (4.00%)
+//!   1 (1.00%) high mild
+//!   3 (3.00%) high severe
+//!
+//! nom_parser              time:   [185.51 ns 186.16 ns 186.95 ns]
+//!                         change: [−7.0377% −6.2565% −5.4446%] (p = 0.00 < 0.05)
+//!                         Performance has improved.
+//! Found 7 outliers among 100 measurements (7.00%)
+//!   3 (3.00%) high mild
+//!   4 (4.00%) high severe
+//! ```
 
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, criterion_group, criterion_main};
 use log::debug;
 
 use dices_rs::dice::parse::parse_with_bonus;
